36e7de9767b7874e56e1d04e6b855882a87d7fe0
Mustafa Homsi <your-homsi6313@gmail.com>
2026-01-20 13:14:46 +0100

    first commit

---
+++ task-1/leap-year.js
@@ -0,0 +1,15 @@
+import promptSync from 'prompt-sync';
+
+const prompt = promptSync();
+//step 1: ask the user for a year
+const inpuut = prompt("Enter a year: ");
+//step 2: convert the input to a number
+const year = parseInt(inpuut);
+//step 3: check if the year is a leap year
+if (year < 1 || year > 9999 || isNaN(year)) {
+    console.log("Invalid year!");
+} else if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    console.log(`yes,${year} is a leap year.`);
+} else {
+    console.log(`no,${year} is not a leap year.`);
+}
